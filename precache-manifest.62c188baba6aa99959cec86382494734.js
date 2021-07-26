265b2350165f7208194e804986f7ba2605106df9
NazarYanchyshyn <yanchishin12@ukr.net>
2021-07-26 16:11:13 +0300

    Updates

---
+++ precache-manifest.62c188baba6aa99959cec86382494734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451cf3b23ed5d7fcb2fa00e737db078d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afc1c028c336b875af6f",
+    "url": "./static/css/main.aa6101d8.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc1c028c336b875af6f",
+    "url": "./static/js/main.b3a81205.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
